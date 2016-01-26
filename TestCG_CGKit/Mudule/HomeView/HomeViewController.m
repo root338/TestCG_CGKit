9ce8c3a984f9d0b4d02e9bd62be75d559c45c4d5
dj <410500921@qq.com>
2016-01-26 16:10:39 +0800

    更新CG_CGKit库

--- TestCG_CGKit/Mudule/HomeView/HomeViewController.m
+++ TestCG_CGKit/Mudule/HomeView/HomeViewController.m
@@ -7,11 +7,14 @@
 //
 
 #import "HomeViewController.h"
-#import "UIView+CGSetupFrame.h"
 
-//#import "TestTableViewController.h"
+#import "UIView+CGSetupFrame.h"
 #import "UIView+CGSetupAppearance.h"
+
+#import "CGButton.h"
 #import "CGLineBoxLayerView.h"
+
+#import "UIApplication+CGVerificationRemoteNotification.h"
 
 @interface HomeViewController ()
 
@@ -45,10 +48,24 @@
 //    self.contentViewHeight.constant = self.view.height;
 //    self.contentViewWidth.constant  = self.view.width;
     
-    CGLineBoxLayerView *layerView = [[CGLineBoxLayerView alloc] initWithFrame:CGRectMake(100, 200, 200, 200)];
-    layerView.backgroundColor = [UIColor orangeColor];
-    [layerView cg_setupBorderWithWidth:1 color:[UIColor redColor]];
-    [self.view addSubview:layerView];
+//    CGLineBoxLayerView *layerView = [[CGLineBoxLayerView alloc] initWithFrame:CGRectMake(100, 200, 200, 200)];
+//    layerView.backgroundColor = [UIColor orangeColor];
+//    [layerView cg_setupBorderWithWidth:1 color:[UIColor redColor]];
+//    [self.view addSubview:layerView];
+//    
+//    [UIApplication cg_verifyUserOpenRemoteNotification];
+    
+    CGButton *button = [CGButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"test button" forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:@"01"] forState:UIControlStateNormal];
+    button.buttonStyle  = CGButtonStyleHorizonalLeft;
+    button.contentAlignment = CGButtonContentAlignmentBottom;
+    button.space        = 8;
+    button.marginEdgeInsets = UIEdgeInsetsMake(8, 8, 8, 8);
+//    button.size     = CGSizeMake(200 , 200);
+    [button sizeToFit];
+    button.origin = CGPointMake(100, 200);
+    [self.view addSubview:button];
 }
 
 - (void)handleTapRecognizer:(UITapGestureRecognizer *)gesture

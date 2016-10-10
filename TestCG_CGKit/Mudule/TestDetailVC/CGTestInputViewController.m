c2cfd8c7585a7534a0e52000e1a056e4442968c0
DY <dy@yuemei.com>
2016-10-10 16:13:14 +0800

    -

--- TestCG_CGKit/Mudule/TestDetailVC/CGTestInputViewController.m
+++ TestCG_CGKit/Mudule/TestDetailVC/CGTestInputViewController.m
@@ -11,6 +11,7 @@
 #import "CGInputAccessoryView.h"
 
 #import "UIView+CGAddConstraints.h"
+#import "NSArray+CGAddConstraints.h"
 
 @interface CGTestInputViewController ()
 {
@@ -25,14 +26,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UITextField *textField  = [[UITextField alloc] init];
-    textField.borderStyle   = UITextBorderStyleRoundedRect;
+    UIView *contentView = [[UIView alloc] init];
+    CGInputAccessoryView *inputAccessoryView    = [[CGInputAccessoryView alloc] initWithFrame:CGRectMake(0, 0, 0, 40)];
     
-    [self.view addSubview:textField];
+    for (NSInteger i = 0; i < 3; i++) {
+        
+        UITextField *textField  = [[UITextField alloc] init];
+        textField.borderStyle   = UITextBorderStyleRoundedRect;
+        [contentView addSubview:textField];
+        textField.inputAccessoryView    = inputAccessoryView;
+    }
+    [self.view addSubview:contentView];
     
-    [textField cg_autoEdgesInsetsZeroToViewController:self exculdingEdge:CGLayoutEdgeBottom];
+    contentView.subviews.subviewsSpaceValue   = 8;
+    [contentView.subviews cg_autoSetupVerticalSubviewsLayout];
+    [contentView.subviews cg_autoDimension:CGDimensionHeight fixedLength:40];
     
-    textField.inputAccessoryView    = [[CGInputAccessoryView alloc] initWithFrame:CGRectMake(0, 0, 0, 40)];
+    [contentView cg_autoEdgesToViewController:self withInsets:UIEdgeInsetsMake(20, 20, 20, 20) exculdingEdge:CGLayoutEdgeBottom];
 }
 
 - (void)didReceiveMemoryWarning {
